2f299b0bef96966a81505efe824b33381f3d476e
ritchie-gr8 <rit.yotsathon@gmail.com>
2024-03-26 11:32:26 +0700

    feat: add answers for all assignments

--- ex-6.js
+++ ex-6.js
@@ -4,3 +4,6 @@ let totalParts = 13000;
 
 let concatSentence;
 let literalSentence;
+
+concatSentence = "Total: " + totalParts + " Machine parts"
+literalSentence = `Total: ${totalParts} Machine parts`
